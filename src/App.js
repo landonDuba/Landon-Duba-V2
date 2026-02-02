716396328a65eb29565a9e723cb2dc31e4067175
Landon Duba <landon@Landons-MacBook-Air.local>
2026-02-02 13:10:31 -0500

    experience section + small changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Card } from "./components/Card";
+import { Experience } from './components/Experience';
 
 function App() {
   const [theme, setTheme] = useState("dark");
@@ -10,10 +11,34 @@ function App() {
     setTheme(prev => (prev === "light" ? "dark" : "light"));
   }
 
+    useEffect(() => {
+      document.body.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const exp_lst = [
+    {pic: "/ibm.png", name: "IBM", title: "Software Engineer Intern", date: "May 2026 - Aug. 2026"},
+    {pic: "/scale.webp", name: "Scale AI", title: "GenAI - Technical Advisor", date: "Oct. 2025 - May. 2026"},
+    {pic: "/mantech.jpg", name: "ManTech", title: "Software Engineer Intern", date: "Dec. 2024 - Mar. 2025"},
+    {pic: "/vt.png", name: "Virginia Tech", title: "CS Major", date: "Aug. 2023 - Dec. 2026"},
+  ]
+
   return (
     <div className="App" data-theme={theme}>
       <Navbar modeSwitch={toggleTheme}/>
       <Card/>
+      <div className="exp">
+        <h1>Experience</h1>
+        {
+          exp_lst.map((curr_exp) => (
+            <Experience exps={curr_exp}/>
+          ))
+        }
+      </div>
+      <hr></hr>
+      <div className='bottom'>
+        <p>contact me at landonduba[at]vt[dot]edu</p>
+        <p>made 2-26</p>
+      </div>
     </div>
   );
 }
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,14 @@
 import './Experience.css'
 
-export function Experience() {
+export function Experience({exps}) {
     return (
-        <></>
+        <div className='experience'>
+            <img src={exps.pic} alt="company/school logo"></img>
+            <div className='inner'>
+                <h4>{exps.name}</h4>
+                <h3>{exps.title}</h3>
+                <h5>{exps.date}</h5>
+            </div>
+        </div>
     )
 }
